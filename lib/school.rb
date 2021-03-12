7488630f8f3600aea5a56cbc1478c286de1e62f8
Muhammad <muhammadhullah23@gmail.com>
2021-03-12 02:44:52 -0500

    commit tree 18db0dbdd8e0faa28f670b187a3a3a520bed2629

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+require "pry"
+class School
+    attr_accessor :name, :roster, :student_grade
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, student_grade)
+        @roster[student_grade] ||= []
+        @roster[student_grade] << student_name
+    end
+
+    def grade(student_grade)
+        @roster[student_grade]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            students.sort!
+        end
+    end
+end
